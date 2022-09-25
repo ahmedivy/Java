49cc12fcda93a83884875f4882fc34759c21f68e
ahmad <91587113+ahmad4919@users.noreply.github.com>
2022-09-25 12:05:42 +0500

    Updated Plot Class

--- Plot/Plot.java
+++ Plot/Plot.java
@@ -4,11 +4,49 @@ public class Plot {
     private Person owner;
     private Address address;
     private Dimension dimension;
+    private int cost;
 
-    public Plot(Person owner, Address address, Dimension dimension){
+    public Plot(Person owner, Address address, Dimension dimension, int cost) {
         this.owner = owner;
         this.address = address;
         this.dimension = dimension;
+        this.cost = cost;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+    
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public double Area() {
+        return dimension.getLength() * dimension.getWidth();
     }
 
 }
