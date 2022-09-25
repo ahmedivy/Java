aa3e8265d0b16f6af55d4e506e8c4635a982f5ea
Ahmed Abdullah <91587113+ahmedivy@users.noreply.github.com>
2022-09-25 11:45:34 +0500

    Updated Person Class

--- Plot/Person.java
+++ Plot/Person.java
@@ -7,13 +7,45 @@ public class Person {
     private Address address;
     private Date dob; 
 
-    public Person(String firstName, String lastName, String gender, String cnic, String cell, Address address){
+    public Person(String firstName, String lastName, String gender, String cnic, String phoneNumber, Date dob){
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
         this.cnic = cnic;
-        this.phoneNumber = cell;
+        this.phoneNumber = phoneNumber;
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCnic() {
+        return cnic;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public String toString() {
+        return String.format("Name: %s %s\nGender: %s\nCNIN: %s\nCell: %s\n DOB: %s", getName(), gender, cnic, phoneNumber, dob);
     }
 
 }
