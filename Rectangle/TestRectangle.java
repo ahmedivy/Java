e08b17a32d787c0ccfdd6aae7c1e439f886a75ff
ahmad <91587113+ahmad4919@users.noreply.github.com>
2022-09-26 15:50:05 +0500

    Checked Bug

--- Rectangle/TestRectangle.java
+++ Rectangle/TestRectangle.java
@@ -1,8 +1,8 @@
 public class TestRectangle {
     public static void main(String[] args) {
 
-        Rectangle r1 = new Rectangle(4, 12, 4, 3);
-        Rectangle r2 = new Rectangle(4, 12, 4, 3);
+        Rectangle r1 = new Rectangle(4, 12, 8, 7);
+        Rectangle r2 = new Rectangle(10, 4, 19, 4);
 
         System.out.println(r1.overlaps(r2) ? "Rectangles Overlap" : "Rectangles do not overlap");
         System.out.println(r1.intersection(r2));
