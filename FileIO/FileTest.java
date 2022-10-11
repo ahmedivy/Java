6b361bce44a95efc3a9b8cb200cfd97521659ba5
ahmad <ahmadabdullah4919@gmail.com>
2022-10-11 17:57:10 +0500

    FilesIO Manipulation

---
+++ FileIO/FileTest.java
@@ -0,0 +1,31 @@
+package FileIO;
+
+import java.io.File;
+
+public class FileTest {
+    
+    // Created a file object
+    static File file = new File("E:\\Java\\BookStore");
+    public static void main(String[] args) {
+        
+        // Some methods of File class
+        System.out.println("File Name: " + file.getName());
+        System.out.println("File Path: " + file.getPath());
+        System.out.println("File Absolute Path: " + file.getAbsolutePath());
+        System.out.println("File Parent: " + file.getParent());
+        System.out.println("File Exists: " + file.exists());
+        // Whether it is a directory
+        System.out.println("File is Directory: " + file.isDirectory());
+        // Whether it is a file
+        System.out.println("File is File(means not a folder): " + file.isFile());
+        // Whether it is hidden
+        System.out.println("File is Hidden: " + file.isHidden());
+        
+        // Print all the files in the directory(if it is a directory)
+        String[] filesList = file.list();
+        System.out.println("Files List: ");
+        for (String file : filesList) {
+            System.out.println("\t" + file);
+        }
+    }
+}
