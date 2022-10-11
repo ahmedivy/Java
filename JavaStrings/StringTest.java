57fbf1d54fedde5ea01045513abed017248797e0
ahmad <ahmadabdullah4919@gmail.com>
2022-10-11 17:26:00 +0500

    String Manipulations

--- JavaStrings/StringTest.java
+++ JavaStrings/StringTest.java
@@ -2,7 +2,10 @@ package JavaStrings;
 
 public class StringTest {
     public static void main(String[] args) {
+        
         String str = "This is a test.";
+
+        // Some String methods:
         System.out.println("Length of str: " + str.length());
         System.out.println("The character at position 3: " + str.charAt(3));
         System.out.println("The substring from 4 to 9: " + str.substring(4, 9));
@@ -16,6 +19,8 @@ public class StringTest {
         System.out.println("Replace 'a' with 'X': " + str.replace('a', 'X'));
         System.out.println("Replace \"test\" with \"toast\": " + str.replace("test", "toast"));
         System.out.println("Remove leading and trailing whitespace: " + ("     " + str + "     ").trim());
+        
+        // Strings Regex Methods:
         // System.out.println("Remove leading whitespace: " + ("     " + str + "     ").trim().replaceFirst("^\\s+", ""));
         // System.out.println("Remove trailing whitespace: " + ("     " + str + "     ").trim().replaceFirst("\\s+$", ""));
     }
